90df197a9aa15f9780a18b303960723c6c851304
Habchi Abdennour <habchi.abdennour.17@gmail.com>
2025-07-25 17:49:27 -0800

    learn Get and Post

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
-app=FastAPI()
+app = FastAPI()
+
 
 
 @app.get("/")
-def root():
-    return {"hello world"}
+async def run_tasks():
+    
+    return {"task1":"welcome to my endpoint"}
+
+@app.post('/creatpost')
+def creat(payload:dict=Body(...)):
+    context={
+        "title ":payload["title"],
+        "content ":payload["content"]
+    }
+    return context
