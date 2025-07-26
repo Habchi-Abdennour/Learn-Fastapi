bee46bc7107fd5f19315f2ce2a0768ee8cc18235
Habchi Abdennour <habchi.abdennour.17@gmail.com>
2025-07-26 12:06:39 -0800

    dealing with from pydantic import BaseModel

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
-
+from pydantic import BaseModel
 app = FastAPI()
 
+class  Post(BaseModel):
+    title:str
+    content:str
+    published:bool =True
+    rating:Optional[int]=None
 
 
 @app.get("/")
@@ -10,10 +16,11 @@ async def run_tasks():
     
     return {"task1":"welcome to my endpoint"}
 
+
 @app.post('/creatpost')
-def creat(payload:dict=Body(...)):
+def creat(new_post:Post):
+    
     context={
-        "title ":payload["title"],
-        "content ":payload["content"]
+        "data":new_post
     }
     return context
